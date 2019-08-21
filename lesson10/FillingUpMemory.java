d98e3b9ff2d120a7df23cc53c5f5d5097488caaf
LukaszSpyra <lukasz.spyrka@gmail.com>
2019-08-21 07:56:55 +0200

    Adding thread excercise and minor update to filling up memory

--- lesson10/FillingUpMemory.java
+++ lesson10/FillingUpMemory.java
@@ -1,8 +1,11 @@
 package pl.samouczekprogramisty.lesson10;
 
+
 public class FillingUpMemory {
     public static void main(String[] args) {
         long a = 9990000000000L;
         long[][] arrayLong = new long [200000][20000];
+
+
     }
 }
